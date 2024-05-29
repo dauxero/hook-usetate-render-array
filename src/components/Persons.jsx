bf548f9dded6406ef8c71b3421aa387748445d00
carlos alberto gonzalez <carlos.gonzalez923@gmail.com>
2024-05-28 21:58:50 -0500

    width modal

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -133,7 +133,7 @@ const Persons = ({ persons, setPersons }) => {
 
       {showModal ? (
         <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="relative w-[600px] my-6 mx-auto max-w-3xl">
             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
               <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                 <h3 className="text-3xl font-semibold">Modal de eliminacion</h3>
